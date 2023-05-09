bc11fba6edd136371b37aab901a386894170801f
Alisa <alisa@side.one>
2023-05-09 21:18:39 +0800

    feat: swap form

---
+++ components/swap/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-96 bg-base-100 mt-8 mx-auto rounded shadow p-4">
+    <div class="flex items-center justify-between mb-4">
+      <div class="text-xl font-semibold flex-1">Swap</div>
+      <div class="btn btn-ghost btn-sm">
+        <Icon
+          name="streamline:interface-setting-slider-horizontal-adjustment-adjust-controls-fader-horizontal-settings-slider"
+          size="16"
+        />
+      </div>
+    </div>
+
+    <div class="flex items-center text-xs justify-between text-gray-400">
+      <div>From</div>
+      <div>BALANCE: 0</div>
+    </div>
+  </div>
+</template>
