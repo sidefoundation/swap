21eee881564f4ab197359fec3a1c25eca27f688d
Alisa <alisa@side.one>
2023-05-09 21:40:25 +0800

    feat: theme

--- components/base/NavTop.vue
+++ components/base/NavTop.vue
@@ -37,12 +37,10 @@
       </ul>
     </div>
     <div class="navbar-end">
-      <h1>Color mode: {{ $colorMode.value }}</h1>
       <select v-model="$colorMode.preference">
         <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
-        <option value="sepia">Sepia</option>
       </select>
       <a class="btn">Get started</a>
     </div>
